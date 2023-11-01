9774d27e7aad45ed81465092eb112f7dd9b95dba
SantosRobertin <robertin.almeida.santos@gmail.com>
2023-11-01 11:03:25 -0300

    melhorias de acessibilidade e reduzindo o tamanho das imagens

--- scripts/index.js
+++ scripts/index.js
@@ -1,66 +1,69 @@
-const elemProjects = document.getElementById('project__content')
+const elemProjects = document.getElementById("project__content");
 
-const createImage = (projectImage) => {
-    const elemPicture = document.createElement('picture')
-    const elemImg = document.createElement('img')
+const createImage = (projectImage, projectName) => {
+  const elemPicture = document.createElement("picture");
+  const elemImg = document.createElement("img");
 
-    elemImg.setAttribute('src', projectImage)
+  elemImg.setAttribute("src", projectImage);
+  elemImg.setAttribute("alt", "Imagem de capa do projeto" + projectName);
 
-    elemPicture.appendChild(elemImg)
+  elemPicture.appendChild(elemImg);
 
-    return elemPicture
-}
+  return elemPicture;
+};
 
 const createStrong = (projectName) => {
-    const elemStrong = document.createElement('strong')
-    elemStrong.innerText = projectName
+  const elemStrong = document.createElement("strong");
+  elemStrong.innerText = projectName;
 
-    return elemStrong
-}
+  return elemStrong;
+};
 
 const createTags = (projectTags) => {
-    const elemTags = document.createElement('div')
-        
-    projectTags.forEach(tag => {
-        const elemTag = document.createElement('span')
-        elemTag.innerText = tag
+  const elemTags = document.createElement("div");
 
-        elemTags.appendChild(elemTag)
-    })
+  projectTags.forEach((tag) => {
+    const elemTag = document.createElement("span");
+    elemTag.innerText = tag;
 
-    return elemTags
-}
+    elemTags.appendChild(elemTag);
+  });
+
+  return elemTags;
+};
 
 const createProject = (project, index) => {
-    const elemProject = document.createElement('a')
+  const elemProject = document.createElement("a");
 
-    elemProject.setAttribute('href', project.link)
-    elemProject.setAttribute('target', '_blank')
+  elemProject.setAttribute("href", project.link);
+  elemProject.setAttribute("target", "_blank");
 
-    elemProject.setAttribute('data-aos', 'zoom-in-up')
-    elemProject.setAttribute('data-aos-duration', '800')
-    elemProject.setAttribute('data-aos-easing', 'ease-in-out')
-    elemProject.setAttribute('data-aos-offset', '-100')
-    elemProject.setAttribute('data-aos-delay', 300 * (index + 1))
-       
-    elemProject.classList.add('project')
+  elemProject.setAttribute("data-aos", "zoom-in-up");
+  elemProject.setAttribute("data-aos-duration", "800");
+  elemProject.setAttribute("data-aos-easing", "ease-in-out");
+  elemProject.setAttribute("data-aos-offset", "-100");
+  elemProject.setAttribute("data-aos-delay", 300 * (index + 1));
 
-    // add imagem de capa
-    elemProject.appendChild(createImage(project.image))        
-        
-    // add nome do projeto
-    elemProject.appendChild(createStrong(project.name))
+  elemProject.classList.add("project");
 
-    // add tags do projeto
-    elemProject.appendChild(createTags(project.tags))
+  // add imagem de capa
+  elemProject.appendChild(createImage(project.image, project.name));
 
-    return elemProject
-}
+  // add nome do projeto
+  elemProject.appendChild(createStrong(project.name));
+
+  // add tags do projeto
+  elemProject.appendChild(createTags(project.tags));
+
+  return elemProject;
+};
 
 const loadProjects = (projects) => {
-    projects.forEach((project, index) => {
-        elemProjects.appendChild(createProject(project, index))
-    })
-}
+  projects.forEach((project, index) => {
+    elemProjects.appendChild(createProject(project, index));
+  });
+};
 
-fetch('./projects.json').then(response => response.json()).then(loadProjects)
+fetch("./projects.json")
+  .then((response) => response.json())
+  .then(loadProjects);
